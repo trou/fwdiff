749e2d1aea27ef2b7832bd4d52304b7991195655
Raphaël Rigo <raphael.rigo@airbus.com>
2019-06-12 14:52:47 +0200

    cleanup, storage

--- diff_tree.sh
+++ diff_tree.sh
@@ -6,37 +6,52 @@ NC='\033[0m' # No Color
 BINUTILS_PREFIX="powerpc-linux-gnu-"
 
 make_idb () {
-    echo "Creating IDB for $1"    
+    echo "Creating IDB for $1"
     if [ ! -f "$1.idb" ]; then
         size=$(stat -c%s "$1")
         if [ $((size)) -gt $((10*1024*1024)) ]; then
             echo "file too big"
         else
-            TVHEADLESS=1 ida -B -A "$1"
-            export_bindiff.sh "$1.idb"
             dest=$(readlink -f "$(dirname "$1")")
-            ida -Llog -A "-OBinExportAutoAction:BinExportBinary" "-OBinExportModule:$dest/" "-S$HOME/.idapro/mybinexport.idc" "$1"
+            TVHEADLESS=1 ida -Llog -B -A "-OBinExportAutoAction:BinExportBinary" "-OBinExportModule:$dest/" "-S$HOME/.idapro/mybinexport.idc" "$1"
         fi
     fi
 }
 
-(rsync --links -rcn --out-format="%n" "$1" "$2" && rsync --links -rcn --out-format="%n" "$2" "$1") | sort | uniq > difflist
-cat difflist | sed "s;^;$1;" | xargs file | grep ELF | cut -d':' -f 1 | sed "s;^$1;;" > diff_elf
-cat diff_elf | while read -r elf ; do 
+if [ $# -lt 3 ]; then
+    echo "Usage: $0 storage_path old new"
+    exit 1
+fi
+
+store=$1
+old=$2
+new=$3
+
+if [ ! -f "$store/difflist" ] ; then
+    (rsync --links -rcn --out-format="%n" "$old" "$new" && rsync --links -rcn --out-format="%n" "$new" "$old") | sort | uniq > "$store/difflist"
+    cat "$store/difflist" | sed "s;^;$old;" | xargs file | grep ELF | cut -d':' -f 1 | sed "s;^$old;;" > "$store/diff_elf"
+    cat "$store/difflist" | sed "s;^;$old;" | xargs file | grep -v ELF | cut -d':' -f 1 | sed "s;^$old;;" > "$store/diff_not_elf"
+fi
+cat "$store/diff_elf" | while read -r elf ; do
     tmp=$(mktemp)
-    ${BINUTILS_PREFIX}objcopy -j .text -O binary "$1/$elf" "$tmp"
+    ${BINUTILS_PREFIX}objcopy -j .text -O binary "$old/$elf" "$tmp"
     md5_1=$(md5sum "$tmp" |cut -d' ' -f1)
-    ${BINUTILS_PREFIX}objcopy -j .text -O binary "$2/$elf" "$tmp"
+    ${BINUTILS_PREFIX}objcopy -j .text -O binary "$new/$elf" "$tmp"
     md5_2=$(md5sum "$tmp" |cut -d' ' -f1)
     if [ "$md5_1" == "$md5_2" ]; then
         #echo -e "$elf : ${GREEN}SAME${NC} .text"
         true
     else
         echo -e "$elf : ${RED}DIFF${NC} .text"
-        make_idb "$1/$elf"
-        make_idb "$2/$elf"
-        bindiff --primary="$1/${elf%.*}.BinExport" --secondary="$2/${elf%.*}.BinExport"
+        make_idb "$old/$elf"
+        make_idb "$new/$elf"
+        filename=$(basename "$old/$elf")
+        bindiff --primary="$old/${elf%.*}.BinExport" --secondary="$new/${elf%.*}.BinExport" | tee "$store/${filename}_bindiff.txt"
     fi
-    # diff -u <(hd "$1/$elf") <(hd "$2/$elf") |diffstat
+    # diff -u <(hd "$old/$elf") <(hd "$new/$elf") |diffstat
 done
 
+cat "$store/diff_not_elf" | while read -r file ; do
+    filename=$(basename "$old/$file")
+    diff -u "$old/$file" "$new/$file" | tee "$store/${filename}.diff"
+done
